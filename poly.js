7f4ad97f1be1cf76e015bea0518cb651e24e33e2
Tim-Hinnerk Heuer <heuert@landcareresearch.co.nz>
2019-02-26 11:35:45 +1300

    RM-666 Fix right-hand rule and publish.

--- poly.js
+++ poly.js
@@ -1,12 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
-var helpers_1 = require("@turf/helpers");
 var geojsonhint_1 = require("@mapbox/geojsonhint");
 var _1 = require(".");
-var json = JSON.parse(fs_1.readFileSync('poly.json', 'utf8'));
-var geometry = json.features[0].geometry.coordinates;
-var poly = _1.makeValid(helpers_1.polygon(geometry));
+var json = JSON.parse(fs_1.readFileSync('rp/test.json', 'utf8'));
+var poly = _1.makeValid(JSON.parse(json.planBoundaryText));
 // const ks = kinks(poly)
 var hints = geojsonhint_1.hint(poly);
 hints;
--- poly.ts
+++ poly.ts
@@ -4,9 +4,8 @@ import kinks from '@turf/kinks';
 import {hint} from '@mapbox/geojsonhint'
 import { makeValid } from '.';
 
-const json = JSON.parse(readFileSync('poly.json', 'utf8'))
-const geometry = json.features[0].geometry.coordinates
-const poly = makeValid(polygon(geometry))
+const json = JSON.parse(readFileSync('rp/test.json', 'utf8'))
+const poly = makeValid(JSON.parse(json.planBoundaryText))
 // const ks = kinks(poly)
 const hints = hint(poly)
 hints
